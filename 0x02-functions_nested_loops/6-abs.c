aa08a038d03d4259acc472c3bd660fc8eb2fa032
Nkiru <christieswazz@gmail.com>
2023-08-15 15:00:04 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - function to find the absolute value of a number
+ * @a: function parameter
+ * Return: -a or a
+ */
+int _abs(int i)
+{
+	if (i > 0)
+return (i);
+else if (i < 0)
+return (-i);
+else
+return (0);
+}
