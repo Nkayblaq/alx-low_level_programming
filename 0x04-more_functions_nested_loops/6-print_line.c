686b2f1a48857830fff433f8563f50ba5dd93334
Nkiru <christieswazz@gmail.com>
2023-08-17 14:10:25 -0700

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,15 +7,9 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
-		_putchar('\n');
-	else
+	while (n-- > 0)
 	{
-		int i;
-		
-		for (i = 1; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
+		_putchar('_');
 	}
+	_putchar('\n');
 }
-
