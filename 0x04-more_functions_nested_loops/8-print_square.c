271def9e127823a41f287e4f2450bd893a99f0cc
Nkiru <christieswazz@gmail.com>
2023-08-17 16:14:51 -0700

    8-print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - a function that prints a squaere using the '#' character
+ * @size: size of the square
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i =1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
