d1998a41969fa2d5508bbbdd711ead421e8ffb94
Nkiru <christieswazz@gmail.com>
2023-08-17 11:34:27 -0700

    1-isdigit

---
+++ 1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks from 0 to 9
+ * @c: parameter to be checked
+ * Return: 1 on success and 0 on failure
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
