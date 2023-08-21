0b3b5c97fbdf698edbbe2620ab7d82df84761549
Nkiru <christieswazz@gmail.com>
2023-08-21 11:39:13 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a sting with the first character
+ * @str: string to be printed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	
+	}
+	_putchar('\n');
+}
