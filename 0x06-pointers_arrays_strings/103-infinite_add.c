7454435f392795c91723f0efaaaa02508f82a544
Nkiru <christieswazz@gmail.com>
2023-08-24 16:00:36 -0700

    103-infinite_add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,38 +12,49 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int carry = 0;
-	int r_index = size_r - 2;
+	int index = size_r - 2;
 
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
 
 	r[size_r - 1] = '\0';
-	while (r_index >= 0 || carry)
+
+	if (len1 > size_r - 1 || len2 > size_r - 1)
+	{
+		return (0);
+	}
+
+	while (index >= 0 || carry)
 	{
 		int digit1 = (len1 > 0) ? (n1[len1 - 1] - '0') : 0;
 		int digit2 = (len2 > 0) ? (n2[len2 - 1] - '0') : 0;
+
 		int sum = digit1 + digit2 + carry;
 
 		carry = sum / 10;
 
-		if (r_index >= 0)
+		if (index >= 0)
 		{
 			int digit = sum % 10;
 
-			r[r_index] = digit + '0';
-
-			r_index--;
-
+			r[index] = digit + '0';
+			index--;
 		}
-
 		if (len1 > 0)
 			len1--;
 		if (len2 > 0)
 			len2--;
 	}
-
-	if (r_index >= 0)
-		return (r + r_index + 1);
+	if (carry)
+	{
+		if (index >= 0)
+		{
+			r[index] = carry + '0';
+			index--;
+		}
+	}
+	if (index >= 0)
+		return (r + index + 1);
 	else
 		return (r);
 }
