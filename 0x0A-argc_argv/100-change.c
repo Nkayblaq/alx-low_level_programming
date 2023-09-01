1d8ccd380292d25958783a58c9eefd58d6cdc52f
Nkiru <christieswazz@gmail.com>
2023-09-01 15:18:20 -0700

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,14 +11,14 @@
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	cents = atoi(argv[1]);
-	
+
 	while (cents > 0)
 	{
 		coins++;
