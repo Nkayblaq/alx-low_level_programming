485853775955dce514f9e2cd341d2c31da5f2f9e
Nkiru <christieswazz@gmail.com>
2023-09-04 19:50:09 -0700

    1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,14 +14,14 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 
 	copy = malloc(sizeof(char) + count  + 1);
 
 	if (copy == NULL)
 		return (NULL);
-	for (i = 0; str[i] != 0; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		copy[i] = str[i];
 
 	return (copy);
