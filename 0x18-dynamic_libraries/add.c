583cd61ee467569c5cff310c4a25f2e7fd527b81
Nkiru Arinze <christieswazz@gmail.com>
2024-01-15 15:27:13 -0800

    100-operations

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - add two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
